a0ed62c6851aac27e8abed3dba026454ed4ed822
Guilherme Leite <guipleite@gmail.com>
2019-04-23 10:23:41 -0300

    initial commit

---
+++ classes.py
@@ -0,0 +1,109 @@
+import math
+# from chempy import balance_stoichiometry , Equilibrium
+
+class Metal:
+
+    def __init__(self,nome,ered,coef,eletrons,mmolar):
+        self.Nome = nome
+        self.Ered = ered
+        self.Coef = coef
+        self.Eletrons = eletrons
+        self.MMolar = mmolar
+
+
+class Solucoes:
+
+    def __init__(self,concetracao):
+        self.Concetracao = float(concetracao)
+        # self.Ion1 = Ion1
+        # self.Ion2 = ion2
+
+class Pilha:
+
+    def __init__(self,metal1,metal2,solucao1,solucao2,temperatura):
+        self.Metal1 = metal1
+        self.Metal2 = metal2
+        self.Solucao1 = solucao1
+        self.Solucao2 = solucao2
+        self.Temp = int(temperatura) + 273 # C para K
+
+
+    def catado_anodo(self):
+        if(self.Metal1.Ered>self.Metal2.Ered):
+            self.catodo = self.Metal1
+            self.sol_catodo = self.Solucao1
+            self.anodo = self.Metal2
+            self.sol_anodo = self.Solucao2
+            self.E = self.Metal1.Ered - self.Metal2.Ered
+        else:
+            self.catodo = self.Metal2
+            self.sol_catodo = self.Solucao2
+            self.anodo = self.Metal1
+            self.sol_anodo = self.Solucao1
+            self.E = self.Metal2.Ered - self.Metal1.Ered
+
+    def coeficientes(self):
+        if self.catodo.Eletrons==self.anodo.Eletrons:
+            self.coef_catodo = self.catodo.Coef
+            self.coef_anodo = self.anodo.Coef
+        elif (self.catodo.Eletrons%self.anodo.Eletrons==0):
+            self.coef_catodo = self.catodo.Coef
+            self.coef_anodo = self.catodo.Eletrons
+        elif (self.anodo.Eletrons%self.catodo.Eletrons==0):
+            self.coef_anodo = self.anodo.Coef
+            self.coef_catodo = self.anodo.Eletrons
+        else:
+            self.coef_catodo = self.anodo.Eletrons
+            self.coef_anodo = self.catodo.Eletrons
+
+
+
+
+
+    def calcula_ddp(self):
+
+        R = 8.31
+        F = 96500
+        n = self.anodo.Eletrons + self.catodo.Eletrons
+
+        self.E0 = self.E - ((R*self.Temp)/(n*F)) * math.log( ((self.sol_anodo.Concetracao)**self.coef_anodo )/
+                                                   ((self.sol_catodo.Concetracao)**self.coef_catodo ) )
+
+
+    def calcula_cap_carga(self):
+        # capacidade de carga é número de mols de elétrons vezes a
+        # carga em coulomb de 1 eletrons(xNumero de avogrado) %3600(As(C) para Ah)
+        n = self.anodo.Eletrons + self.catodo.Eletrons
+        self.CapCarga = (n*1.6021E-19*6.0225E23)/3600
+
+    def calcula_densidade_energia(self,massa_eletrodo1,massa_eletrodo2):
+
+        Pothora = self.CapCarga * self.E0
+        # Pothora * 3600 = Joule
+        self.DensEnergia = ( (Pothora * 3600) ) / (float(massa_eletrodo1)+float(massa_eletrodo2))
+
+
+
+# class Bateria:
+#
+#     def __init__(self,DDP,potencia,Cap_Carga):
+#         self.ddp = ddp
+#         self.pot = potencia
+#         self.temp_lig = tempo_ligado
+#         self.cap_carga = Cap_Carga
+
+
+
+
+Ferro2 = Metal("Ferro(2+)",-0.44,1,2,56)
+Ferro3 = Metal("Ferro(3+)",-0.04,1,3,56)
+Cobre = Metal("Cobre",0.34,1,2,63.546)
+Cromo = Metal("Cromo",-0.74,1,3,52)
+Litio = Metal("Lítio",-3.04,1,1,6.94)
+Niquel = Metal("Níquel",-0.23,1,2,58.7)
+Chumbo = Metal("Chumbo",-0.13,1,2,207)
+Prata = Metal("Prata",0.80,1,1,108)
+Cobalto = Metal("Cobalto",-0.28,1,2,59)
+Aluminio = Metal("Alumínio",-1.66,1,3,27)
+
+Metais = [Ferro2,Ferro3,Cobre,Cromo,Litio,Niquel,Chumbo,Prata,Cobalto,Aluminio]
---
+++ main.py
@@ -0,0 +1,215 @@
+import tkinter as tk
+from tkinter import *
+from classes import *
+
+class MainWindow(tk.Frame):
+    counter = 0
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.button = tk.Button(self, text="Montar Pilha", width=15,bg='brown',fg='white',
+                                command=self.create_window)
+        self.button.place(x=200,y=130)
+        self.button = tk.Button(self, text="Selecionar Pilha", width=15,bg='brown',fg='white',
+                                command=self.select_window)
+        self.button.place(x=200,y=230)
+
+
+    def create_window(self):
+        t = tk.Toplevel(self)
+        t.geometry('500x500')
+        t.wm_title("Montar Pilha")
+
+        listMetal = ['Ferro(2+)','Ferro(3+)','Cobre','Cromo','Lítio','Níquel','Chumbo','Prata','Cobalto','Alumínio'];
+
+        label_0 = Label(t, text="Montar Pilha",width=20,font=("bold", 20))
+        label_0.place(x=90,y=53)
+
+        self.met1=StringVar()
+        droplist=OptionMenu(t,self.met1, *listMetal)
+        droplist.config(width=15)
+        self.met1.set('Metal 1')
+        droplist.place(x=192,y=130)
+
+        self.met2 = StringVar()
+        droplist=OptionMenu(t,self.met2, *listMetal)
+        droplist.config(width=15)
+        self.met2.set('Metal 2')
+        droplist.place(x=192,y=180)
+
+        label_1 = Label(t, text="Concentração da Solução do metal 1 em mol/L",width=37,font=("bold", 9))
+        label_1.place(x=20,y=230)
+
+        self.entry_1 = Entry(t)
+        self.entry_1.place(x=280,y=230)
+
+        label_2 = Label(t, text="Concentração da Solução do metal 2 em mol/L",width=37,font=("bold", 9))
+        label_2.place(x=20,y=280)
+
+        self.entry_2 = Entry(t)
+        self.entry_2.place(x=280,y=280)
+
+
+        label_3 = Label(t, text="Temperatura em graus Celcius",width=30,font=("bold", 10))
+        label_3.place(x=40,y=330)
+
+        self.entry_3 = Entry(t)
+        self.entry_3.place(x=280,y=330)
+
+        label_4 = Label(t, text="Massa do Metal 1 em kg",width=30,font=("bold", 10))
+        label_4.place(x=40,y=380)
+
+        self.entry_4 = Entry(t)
+        self.entry_4.place(x=280,y=380)
+
+        label_5 = Label(t, text="Massa do Metal 2 em kg",width=30,font=("bold", 10))
+        label_5.place(x=40,y=430)
+
+        self.entry_5 = Entry(t)
+        self.entry_5.place(x=280,y=430)
+
+
+        Button(t, text='Montar Pilha',width=20,bg='brown',fg='white',command=self.crtd_window).place(x=180,y=470)
+
+    def crtd_window(self):
+    	## Referencia as entradas da tela anterior
+        temp = self.entry_3.get()
+        c2 = self.entry_2.get()
+        c1 = self.entry_1.get()
+        m1 = self.met1.get()
+        m2 = self.met2.get()
+        mm1 = self.entry_4.get()
+        mm2 = self.entry_5.get()
+
+        for metal in Metais:
+            if (m1 == metal.Nome):
+                metal1 = metal
+            if (m2 == metal.Nome):
+                metal2 = metal
+
+        sol1 = Solucoes(c1)
+        sol2 = Solucoes(c2)
+        pilha = Pilha(metal1,metal2,sol1,sol2,temp)
+
+        pilha.catado_anodo()
+
+        pilha.coeficientes()
+
+        pilha.calcula_ddp()
+
+        pilha.calcula_cap_carga()
+
+        pilha.calcula_densidade_energia(mm1,mm2)
+
+        potencia_pilha = round(pilha.E0 * pilha.CapCarga , 2)
+
+        c = tk.Toplevel(self)
+        c.geometry('500x500')
+        c.wm_title("Pilha criada")
+
+        ##Variaveis que serao mostradas
+        ddp = pilha.E
+        esp = pilha.E0
+        capa = round(pilha.CapCarga , 2)
+        densi = pilha.DensEnergia
+        densi = round(densi,2)
+
+        label_0 = Label(c, text="Pilha Criada",width=20,font=("bold", 20))
+        label_0.place(x=90,y=53)
+
+        label_ddp = Label(c, text="DDP padrão",width=20,font=("bold", 10))
+        label_ddp.place(x=20,y=130)
+        label_ddp2 = Label(c, text=(ddp,"Volts"),width=30,font=("bold", 10))
+        label_ddp2.place(x=180,y=130)
+
+        label_esp = Label(c, text="DDP especifica",width=20,font=("bold", 10))
+        label_esp.place(x=20,y=180)
+        label_esp2 = Label(c, text=(esp,"Volts"),width=30,font=("bold", 10))
+        label_esp2.place(x=180,y=180)
+
+        label_capa = Label(c, text="Capacidade de Carga",width=20,font=("bold", 10))
+        label_capa.place(x=20,y=230)
+        label_capa2 = Label(c, text=(capa,"mAh"),width=30,font=("bold", 10))
+        label_capa2.place(x=180,y=230)
+
+        label_pot = Label(c, text="Potência",width=20,font=("bold", 10))
+        label_pot.place(x=20,y=280)
+        label_pot2 = Label(c, text=(potencia_pilha,"W"),width=30,font=("bold", 10))
+        label_pot2.place(x=180,y=280)
+
+        label_densi = Label(c, text="Densidade de Energia",width=20,font=("bold", 10))
+        label_densi.place(x=20,y=330)
+        label_densi2 = Label(c, text=(densi,"J/kg"),width=30,font=("bold", 10))
+        label_densi2.place(x=180,y=330)
+
+    def select_window(self):
+        s = tk.Toplevel(self)
+        s.geometry('500x500')
+        s.wm_title("Selecionar Pilha")
+
+        label_0 = Label(s, text="Selecionar Pilha",width=20,font=("bold", 20))
+        label_0.place(x=90,y=53)
+
+        label_ddp = Label(s, text="D.D.P. em Volts desejada",width=30,font=("bold", 10))
+        label_ddp.place(x=40,y=130)
+
+        entry_ddp = Entry(s)
+        entry_ddp.place(x=280,y=130)
+
+        label_pot = Label(s, text="Potência desejada ",width=30,font=("bold", 10))
+        label_pot.place(x=40,y=180)
+
+        entry_pot = Entry(s)
+        entry_pot.place(x=280,y=180)
+
+        label_cap = Label(s, text="Capacidade de Carga desejada",width=30,font=("bold", 10))
+        label_cap.place(x=40,y=230)
+
+        entry_cap = Entry(s)
+        entry_cap.place(x=280,y=230)
+
+        label_temp = Label(s, text="Tempo de duração",width=30,font=("bold", 10))
+        label_temp.place(x=40,y=280)
+
+        entry_temp = Entry(s)
+        entry_temp.place(x=280,y=280)
+
+        Button(s, text='Procurar Pilha',width=20,bg='brown',fg='white',command=self.found_window).place(x=180,y=470)
+
+    def found_window(self):
+        s = tk.Toplevel(self)
+        s.geometry('500x500')
+        s.wm_title("Selecionar Pilha")
+
+        ddp = self.entry_ddp.get()
+        pot = self.entry_pot.get()
+        cap = self.entry_cap.get()
+        time = self.entry_temp.get()
+
+        Button(s, text='Procurar Pilha',width=20,bg='brown',fg='white',command=self.final_window).place(x=180,y=470)
+
+
+    def final_window(self):
+
+        ddp = self.entry_ddp.get()
+        pot = self.entry_pot.get()
+        cap = self.entry_cap.get()
+        time = self.entry_temp.get()
+
+        s = tk.Toplevel(self)
+        s.geometry('500x500')
+        s.wm_title("Pilha Final")
+
+        label_0 = Label(s, text=("Pilha Final",preco),width=20,font=("bold", 20))
+        label_0.place(x=90,y=53)
+
+        label_ddp = Label(s, text="Preço",width=30,font=("bold", 10))
+        label_ddp.place(x=40,y=130)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry('500x500')
+    root.title("Catalogo de Pilhas")
+    main = MainWindow(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.mainloop()
